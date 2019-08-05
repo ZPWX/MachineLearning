a2654561225733e4b19f1d7ac405ac8425792eee
n23w/snow <3157188714@qq.com>
2019-08-05 14:14:23 +0800

    Add files via upload

---
+++ MachineLearning/04-ML-LR/LogisticRegression.py
@@ -0,0 +1,55 @@
+import numpy as np
+import random
+#from scipy.ndimage.measurements import variance
+#from scipy.stats._continuous_distns import alpha
+#@from numpy import gradient
+#from numpy import shape
+#from numpy import shape
+#from scipy.constants.constants import alpha
+
+
+# gradient decent 
+def gradientDescent(x, y, theta, alpha, m, numIterations):
+    xTrans = x.transpose()
+    for i in range(0, numIterations):
+        hypothesis = np.dot(x, theta)
+        loss = hypothesis - y
+        
+        cost = np.sum(loss ** 2)/(2*m)
+        print("Iteration %d | Cost: %f" %(i, cost))
+        
+        gradient = np.dot(xTrans, loss)/m
+        
+        theta = theta - alpha*gradient
+    return theta
+    
+def genData(numPoints, bias, variance):
+    x = np.zeros(shape=(numPoints, 2))
+    y = np.zeros(shape=numPoints)
+    # basically a straight line
+    for i in range(0, numPoints):
+        # bias feature
+        x[i][0] = 1
+        x[i][1] = i
+        # target variable
+        y[i] = (i + bias) + random.uniform(0, 1)*variance
+        
+    return x, y
+
+# print 100 points
+x, y = genData(100, 25, 10)
+#print("x:", x)
+#print("y:", y)
+
+m, n = np.shape(x)
+n_y = np.shape(y)
+
+print("x shape :",str(m),"",str(n))
+print("y length :",str(n_y))
+
+
+numIterations = 100000
+alpha = 0.0005
+theta = np.ones(n)
+theta = gradientDescent(x, y, theta, alpha, m, numIterations)
+print(theta)
--- MachineLearning/04-ML-LR/SimpleLinearRegression.py
+++ MachineLearning/04-ML-LR/SimpleLinearRegression.py
@@ -15,7 +15,11 @@ def fitSLR(x, y):
         
     b1 = numerator/float(dinominator))
     b0 = np.mean(y)/float(np.mean(x))
+    
     return b0, b1
+
+def predict(x, b0, b1):
+    return b0 + x*b1
 
 x = [1, 3, 2, 1, 3]
 y = [14, 24, 18, 17, 27]
